93177c95f15bf1db58ebf9a45ba900339d9c7c12
Liudmyla Makhynia <ludmilam20090929@gmail.com>
2025-05-02 14:32:58 +0300

    fix host

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { usersMiddlewares } from "./users/middlewares";
 export const apiVersion = "1.0.0";
 const app: express.Express = express();
 
-const HOST = "0.0.0.0";
+const HOST = "127.0.0.1";
 const PORT = Number(process.env.PORT) || 8000;
 
 app.use(express.json());
