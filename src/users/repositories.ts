6286a130c0f3043c385aa086669b571eae3d7d50
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-05-28 07:40:14 +0200

    Create repository functions for querying friendships

--- src/users/repositories.ts
+++ src/users/repositories.ts
@@ -37,9 +37,27 @@ export class UsersRepository {
         }
     }
     async getFriendsForUser(userId: number) {
-        return await prisma.user.findMany({})
+        // get all friendship relations initiated by `userId` itself or to him from other user
+        return await prisma.user.findMany({
+            where: {
+                OR: [
+                    { ownFriendships: { some: { fromUserId: userId, isApproved: true } } },
+                    { ownFriendships: { some: { toUserId: userId, isApproved: true } } },
+                ],
+                NOT: {
+                    id: userId
+                }
+            }
+        })
     }
-    async getFriendRequestsForUser(userId: number) { }
+    async getFriendRequestsForUser(userId: number) {
+        // get all friend requests initiated to `userId` together with user which initiated it
+        return await prisma.userFriend.findMany({
+            where: { toUserId: userId, isApproved: false },
+            include: { fromUser: true }
+        })
+    }
+
     async list(): Promise<User[]> {
         return await prisma.user.findMany();
     }
